ba1fe7735d73aaf2f5d02988158690efd980fde9
Perry Waxman <perry@asu.edu>
2016-10-27 01:40:34 -0700

    Added in some assets and overrides for css and js. getting landing page ready

---
+++ js/landing.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Perry Waxman on 10/27/2016.
+ */
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
